b6ea652400cf622ab4f34d5ce5c55ebd270c3d19
Thomas Mayer <thomas.mayer13@outlook.com>
2025-05-23 07:04:53 +0200

    clean up

--- build/Build.cs
+++ build/Build.cs
@@ -1,17 +1,8 @@
-using System;
-using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
-using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
-using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.PathConstruction;
 using Serilog;
-using static System.Net.Mime.MediaTypeNames;
 using Nuke.Common.Tools.Docker;
 using System.IO;
 
